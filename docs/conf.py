bd06f56ce2b42f4ecc432ef5473eeb97ac291f43
Alexeev Bronislav <alexeev.dev@mail.ru>
2026-03-04 00:18:43 +0700

    update docs workflows

--- docs/conf.py
+++ docs/conf.py
@@ -77,8 +77,12 @@ html_context = {
 }
 
 html_theme_options = {
-    "footer_content": f"Documentation version: {version} ({docs_type})",
+    "footer_content": f"Documentation version: {version} ({docs_type}) | "
+                     f"<a href='/nadzoring/'>Stable</a> | "
+                     f"<a href='/nadzoring/latest/'>Latest</a>",
 }
+
+
 
 todo_include_todos = True
 
