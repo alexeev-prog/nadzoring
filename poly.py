792f003a3eb45b0ed76ff3b2a917be030d86c268
Alexeev Bronislav <alexeev.dev@mail.ru>
2026-03-04 14:49:56 +0700

    update docs (fix)

--- poly.py
+++ poly.py
@@ -4,38 +4,24 @@ from pathlib import Path
 from sphinx_polyversion.api import apply_overrides
 from sphinx_polyversion.driver import DefaultDriver
 from sphinx_polyversion.git import Git, GitRef, GitRefType, file_predicate
-from sphinx_polyversion.pyvenv import Pip
 from sphinx_polyversion.sphinx import SphinxBuilder
 
-#: Regex matching the branches to build docs for
 BRANCH_REGEX = r"^main$"
 
-#: Regex matching the tags to build docs for
 TAG_REGEX = r"^v\d+\.\d+.*$"
 
-#: Output dir relative to project root
 OUTPUT_DIR = "docs/_build/html"
 
-#: Source directory relative to project root
 SOURCE_DIR = "docs"
 
-#: Arguments to pass to `sphinx-build`
 SPHINX_ARGS = ["-a", "-v"]
 
-#: Mock data used for building local version (for local testing)
 MOCK_DATA = {
     "revisions": [
         GitRef("main", "", "", GitRefType.BRANCH, datetime.fromtimestamp(0)),
     ],
     "current": GitRef("main", "", "", GitRefType.BRANCH, datetime.fromtimestamp(0)),
 }
-
-#: Pip dependencies to install for building docs
-PIP_PACKAGES = [
-    "sphinx",
-    "furo",
-    "sphinx-polyversion",
-]
 
 apply_overrides(globals())
 
@@ -52,9 +38,4 @@ DefaultDriver(
         source=Path(SOURCE_DIR),
         args=SPHINX_ARGS,
     ),
-    env=Pip(
-        packages=PIP_PACKAGES,
-    ),
-    template_dir=Path("docs/_templates"),
-    static_dir=Path("docs/_static"),
 ).run(MOCK_DATA)
